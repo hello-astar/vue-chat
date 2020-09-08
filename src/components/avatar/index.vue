6f1abf8444e21e5f22a2bced249c8bbf30925542
astar <2320390964@qq.com>
2020-09-08 22:42:17 +0800

    抽出公共组件

---
+++ src/components/avatar/index.vue
@@ -0,0 +1,117 @@
+<!--
+ * @Description: 头像组件
+ * @Author: astar
+ * @Date: 2020-09-08 21:10:10
+ * @LastEditTime: 2020-09-08 22:13:34
+ * @LastEditors: astar
+-->
+<template>
+  <span class="avatar-wrapper" :class="classes" :style="styles">
+    <slot>
+      <img :style="imgStyles" :src="src" :alt="alt" @error="error">
+    </slot>
+  </span>
+</template>
+<script>
+export default {
+  props: {
+    shape: { // 形状
+      type: String,
+      default: 'square',
+      validator(val) {
+        return ['circle', 'square'].includes(val);
+      }
+    },
+    fit: { // fill / contain / cover / none / scale-down
+      type: String,
+      default: 'cover'
+    },
+    size: {
+      type: [Number, String],
+      default: 'medium',
+      validator(val) {
+        if (typeof val === 'string') {
+          return ['large', 'medium', 'small'].includes(val);
+        }
+        return typeof val === 'number';
+      }
+    },
+    src: {
+      type: String,
+      default: ''
+    },
+    alt: String
+  },
+  methods: {
+    error(e) {
+      this.$emit('error', e)
+    }
+  },
+  computed: {
+    classes () {
+      const { size, shape } = this
+      let classList = []
+
+      if (size && typeof size === 'string') {
+        classList.push(`avatar-size__${size}`);
+      }
+
+      if (shape) {
+        classList.push(`avatar-shape__${shape}`);
+      }
+
+      return classList.join(' ');
+    },
+    styles () {
+      const { size } = this
+      return typeof size === 'number' ? {
+        height: `${size}px`,
+        width: `${size}px`,
+        lineHeight: `${size}px`
+      } : {}
+    },
+    imgStyles () {
+      const { fit } = this
+      return {
+        objectFit: fit
+      }
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.avatar-wrapper {
+  display: inline-block;
+  box-sizing: border-box;
+  vertical-align: middle;
+  overflow: hidden;
+  text-align: center;
+  >img {
+    display: block;
+    height: 100%;
+  }
+  &.avatar-size__large {
+    width: 40px;
+    height: 40px;
+    line-height: 40px;
+  }
+  &.avatar-size__medium {
+    width: 36px;
+    height: 36px;
+    line-height: 36px;
+  }
+  &.avatar-size__small {
+    width: 28px;
+    height: 28px;
+    line-height: 28px;
+  }
+  &.avatar-shape {
+    &__circle {
+      border-radius: 50%;
+    }
+    &__square {
+      border-radius: 4px;
+    }
+  }
+}
+</style>
---
+++ src/components/searchBox/index.vue
@@ -0,0 +1,66 @@
+<!--
+ * @Description: 搜索框
+ * @Author: astar
+ * @Date: 2020-09-08 20:54:56
+ * @LastEditTime: 2020-09-08 22:29:04
+ * @LastEditors: astar
+-->
+<template>
+<div class="search-box-wrapper">
+  <slot name="icon">
+    <div class="search-icon"></div>
+  </slot>
+  <input type="text" :placeholder="placeholder" v-model="searchTxt" @input="input">
+</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      searchTxt: ''
+    }
+  },
+  props: {
+    value: String,
+    placeholder: {
+      type: String,
+      default: '搜索'
+    }
+  },
+  watch: {
+    value: function (val) {
+      this.searchTxt = val
+    }
+  },
+  methods: {
+    input (e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.search-box-wrapper {
+  width: 100%;
+  background-color: #26292e;
+  border-radius: 4px;
+  .search-icon {
+    display: inline-block;
+    width: 32px;
+    height: 32px;
+    background: url('~@/assets/images/search.png') no-repeat center;
+    background-size: 25px 25px;
+    vertical-align: middle;
+  }
+  input {
+    display: inline-block;
+    height: 100%;
+    background-color: #26292e;
+    outline: 0;
+    border: none;
+    padding: 0 10px 0 8px;
+    color: rgb(244, 244, 244);
+    vertical-align: middle;
+  }
+}
+</style>
