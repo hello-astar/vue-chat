a6326bfc663c81b1422f0edcc752dfdb9a24a093
cmx <cmx@imlaidian.com>
2020-09-10 18:32:50 +0800

    增加vconsole

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/styles/reset.css";
 import "@/assets/styles/common.scss";
+import VConsole from 'vconsole';
+
+var vConsole = new VConsole();
+console.log(vConsole);
+
 Vue.config.productionTip = false;
 
 new Vue({
