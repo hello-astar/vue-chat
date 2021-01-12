e1561499f8f530906a4c5dc7c22cfc0bcb5bc2bd
astar <2320390964@qq.com>
2021-01-12 18:32:33 +0800

    样式修改

--- src/config/index.js
+++ src/config/index.js
@@ -2,8 +2,8 @@
  * @author: cmx
  * @Date: 2020-09-09 17:51:08
  * @LastEditors: cmx
- * @LastEditTime: 2021-01-11 16:04:32
+ * @LastEditTime: 2021-01-12 17:58:33
  * @Description: 文件描述
  * @FilePath: \vue-chat\src\config\index.js
  */
-export const BASE_URL = '192.168.22.173:3000'
+export const BASE_URL = 'localhost:3000'
