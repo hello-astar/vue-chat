1bf0c9692f5262e788b6e508afaa8caca060d378
astar <2320390964@qq.com>
2021-06-26 23:40:58 +0800

    feat: change production baseURL

--- src/config/index.js
+++ src/config/index.js
@@ -2,13 +2,13 @@
  * @Description: 
  * @Author: astar
  * @Date: 2021-06-26 18:16:11
- * @LastEditTime: 2021-06-26 20:41:32
+ * @LastEditTime: 2021-06-26 23:40:35
  * @LastEditors: astar
  */
 let baseURL = ''
 if (process.env.NODE_ENV === 'development') {
   baseURL = 'https://192.168.0.102:3001'
 } else {
-  baseURL = '//hello-astar.asia'
+  baseURL = '//1.15.88.142'
 }
 export default baseURL
