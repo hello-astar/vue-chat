4355109f8f9dfbdec98a6a54cee568c77fec0dd4
astar <2320390964@qq.com>
2021-07-06 21:28:50 +0800

    style: 404增加回首页按钮

--- src/views/error/404.vue
+++ src/views/error/404.vue
@@ -2,24 +2,31 @@
  * @Author: astar
  * @Date: 2021-02-25 11:35:09
  * @LastEditors: astar
- * @LastEditTime: 2021-07-06 18:43:23
+ * @LastEditTime: 2021-07-06 21:27:24
  * @Description: 文件描述
  * @FilePath: \vue-chat\src\views\error\404.vue
 -->
 <template>
   <div class="page-404">
-    <svg-icon icon-name="404" class="svg-icon"></svg-icon>
+    <div class="page-404__content">
+      <svg-icon icon-name="404" class="svg-icon"></svg-icon>
+      <s-button type="danger" @click="$router.push('/')">回到首页</s-button>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
 .page-404 {
-  .svg-icon {
+  &__content {
     position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     width: 200px;
-    height: 200px;
+    text-align: center;
+    .svg-icon {
+      width: 200px;
+      height: 200px;
+    }
   }
 }
 </style>
