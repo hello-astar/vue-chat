c6f6edf6c10ea4a2014bbb4307dac6ee4b048402
astar <2320390964@qq.com>
2021-07-06 19:46:46 +0800

    fix: invalid url

--- src/config/index.js
+++ src/config/index.js
@@ -2,13 +2,13 @@
  * @Description: 
  * @Author: astar
  * @Date: 2021-06-26 18:16:11
- * @LastEditTime: 2021-07-06 18:01:02
+ * @LastEditTime: 2021-07-06 19:46:20
  * @LastEditors: astar
  */
 let baseURL = ''
 if (process.env.NODE_ENV === 'development') {
   baseURL = 'https://192.168.0.102:3001'
 } else {
-  baseURL = '//hello-astar.asia/api'
+  baseURL = 'https://hello-astar.asia/api'
 }
 export default baseURL
