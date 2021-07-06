eb1dd859fb0066e0bd8eb59f7b3e0d2ab4a87ce7
astar <2320390964@qq.com>
2021-07-06 18:01:31 +0800

    feat: 测试环境不要api

--- src/config/index.js
+++ src/config/index.js
@@ -2,12 +2,12 @@
  * @Description: 
  * @Author: astar
  * @Date: 2021-06-26 18:16:11
- * @LastEditTime: 2021-07-06 17:13:01
+ * @LastEditTime: 2021-07-06 18:01:02
  * @LastEditors: astar
  */
 let baseURL = ''
 if (process.env.NODE_ENV === 'development') {
-  baseURL = 'https://192.168.0.102:3001/api'
+  baseURL = 'https://192.168.0.102:3001'
 } else {
   baseURL = '//hello-astar.asia/api'
 }
