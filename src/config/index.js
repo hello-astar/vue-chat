da1f56bf9c50e1b8d11b97d0ec336ca30faa12d2
astar <2320390964@qq.com>
2021-07-06 17:53:37 +0800

    feat: 接口修改

--- src/config/index.js
+++ src/config/index.js
@@ -2,13 +2,13 @@
  * @Description: 
  * @Author: astar
  * @Date: 2021-06-26 18:16:11
- * @LastEditTime: 2021-06-29 00:07:36
+ * @LastEditTime: 2021-07-06 17:13:01
  * @LastEditors: astar
  */
 let baseURL = ''
 if (process.env.NODE_ENV === 'development') {
-  baseURL = 'https://192.168.0.102:3001'
+  baseURL = 'https://192.168.0.102:3001/api'
 } else {
-  baseURL = '//hello-astar.asia'
+  baseURL = '//hello-astar.asia/api'
 }
 export default baseURL
